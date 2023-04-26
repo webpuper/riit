330987914933d53b5cf4de204a7171cc2f51e362
BzoomTeam <bzoom@live.ru>
2023-04-26 13:41:17 +0300

    Миграции

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->bigInteger('education')->nullable();
             $table->bigInteger('city')->nullable();
             $table->string('email')->unique();
--- database/migrations/2023_04_26_100309_create_education_table.php
+++ database/migrations/2023_04_26_100309_create_education_table.php
@@ -15,7 +15,7 @@ class CreateEducationTable extends Migration
     {
         Schema::create('education', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
         });
     }
 
