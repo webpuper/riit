9d3229f29a142439dca8a4abe1d7135cd4511525
BzoomTeam <bzoom@live.ru>
2023-04-28 14:12:50 +0300

    Рефакторинг

--- public/app/controller/MainController.js
+++ public/app/controller/MainController.js
@@ -30,28 +30,34 @@ Ext.define('appUsers.controller.MainController', {
 
     UpdateGridStore: function () {
         let contr = this;
+        let store = Ext.getStore('personnel');
         contr.setMaskBody(true);
-        Ext.getStore('personnel').sync({
+        store.sync({
             callback: function (records, operation, success) {
                 contr.setMaskBody(false);
             },
             success: function (batch, options) {
                 Ext.toast('Запись успешно обновлена', 'Обновление', 't');
+                if (Ext.getCmp('filter_education_id').getValue()) {
+                    // установлен фильтр -> перезагрузим store
+                    store.reload();
+                }
             },
             failure: function (batch, options) {
                 Ext.toast('Ошибка', 'Обновление', 't');
             }
         });
+
     },
 
     LoadGridStore: function (Params = null) {
         Ext.getStore('personnel').load({
             params: Params,
-            callback: function(records, operation, success) {
-               
+            callback: function (records, operation, success) {
+
             }
         });
-        
+
     },
 
     SelectComboEd: function (combo) {
@@ -63,8 +69,7 @@ Ext.define('appUsers.controller.MainController', {
     // ----------------------------------------------
     // Для фильтров
 
-    getFilterParams: function(filter_education)
-    {
+    getFilterParams: function (filter_education) {
         return {
             filter: true,
             education: filter_education
@@ -75,7 +80,7 @@ Ext.define('appUsers.controller.MainController', {
     FilterEducationClear: function (combo) {
         // Сброс фильтра Образование
         combo.clearValue();
-       
+        Ext.getStore('personnel').load();
     },
 
     FilterEducationSelect: function (combo) {
